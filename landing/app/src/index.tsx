0441b5120c2f3340e872fcb12fddf95223559b57
AlexR0v <mail.ponomarev@yandex.ru>
2020-09-16 13:23:57 +0300

    chore(hiro): add helmet

--- landing/app/src/index.tsx
+++ landing/app/src/index.tsx
@@ -1,4 +1,3 @@
-import Head              from 'next/head'
 import React             from 'react'
 import { ThemeProvider } from 'emotion-theming'
 import { IntlProvider }  from 'react-intl'
@@ -6,18 +5,12 @@ import { IntlProvider }  from 'react-intl'
 import { theme }         from '@ui/theme'
 
 import { App }           from './App'
+import { Seo }           from './Seo'
 
 export default () => (
   <>
-    <Head>
-      <title>Riverdoc</title>
-      <meta charSet='utf-8' />
-      <meta
-        name='viewport'
-        content='width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0'
-      />
-    </Head>
     <IntlProvider locale='ru' defaultLocale='ru'>
+      <Seo />
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
